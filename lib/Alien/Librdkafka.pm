5ef8d6dddd85f296d62a1f7dccc3cef8441301f5
Pavel Shaydo <zwon@cpan.org>
2016-10-14 22:22:15 +0200

    documentation

--- lib/Alien/Librdkafka.pm
+++ lib/Alien/Librdkafka.pm
@@ -10,6 +10,28 @@ our $VERSION = '0.9.1';
 
 Alien::Librdkafka - build and install librdkafka
 
+=head1 DESCRIPTION
+
+The distribution provides the way to build and access librdkafka from perl.
+
+=head1 SEE ALSO
+
+L<Alien::Base>
+
+L<https://github.com/edenhill/librdkafka>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2016 Pavel Shaydo
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+For librdkafka license and copyright check L<https://github.com/edenhill/librdkafka>
+
 =cut
  
 1;
