4ec5c9c2aef4909eb0d7cc8fb24adebeb17dbfcc
Pavel Shaydo <zwon@cpan.org>
2016-10-14 22:21:50 +0200

    initial implementation

---
+++ lib/Alien/Librdkafka.pm
@@ -0,0 +1,15 @@
+package Alien::Librdkafka;
+ 
+use strict;
+use warnings;
+ 
+use parent 'Alien::Base';
+our $VERSION = '0.9.1';
+
+=head1 NAME
+
+Alien::Librdkafka - build and install librdkafka
+
+=cut
+ 
+1;
