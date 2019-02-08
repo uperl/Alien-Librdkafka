246d43d9a67869445ea3162a1fa80e0884356e14
Graham Ollis <plicease@cpan.org>
2019-02-08 17:41:25 -0500

    new maintainer

--- lib/Alien/Librdkafka.pm
+++ lib/Alien/Librdkafka.pm
@@ -14,16 +14,17 @@ Alien::Librdkafka - build and install librdkafka
 
 The distribution provides the way to build and access librdkafka from perl.
 
-=head1 SUPPORT
-
-Please note that this module is not actively developed and maintained, if you
-are interested in taking it over, please contact the author
-
 =head1 SEE ALSO
 
 L<Alien::Base>
 
 L<https://github.com/edenhill/librdkafka>
+
+=head1 AUTHOR
+
+The Original author is Pavel Shaydo
+
+The current maintainer is Graham Ollis <plicease@cpan.org>
 
 =head1 LICENSE AND COPYRIGHT
 
